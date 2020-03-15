8e175642acfd8d7c0fdfd7f5771f65f5c252d2e3
Otomaru3 <38760547+Otomaru3@users.noreply.github.com>
2020-03-16 08:56:47 +0900

    Update iptables_allowjpipv4.sh

--- iptables_allowjpipv4.sh
+++ iptables_allowjpipv4.sh
@@ -38,7 +38,8 @@ iptables -F
 
 
 echo ! IPTABLES 固定設定
-iptables -A INPUT -m state -state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+#iptables -A INPUT -m state -state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 
